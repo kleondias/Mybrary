305f15b4cbbb039374a869523d56629120e57ecd
Kleon Dias <kdias76@hotmail.com>
2020-04-30 10:38:56 +0300

    Initial Setup

---
+++ server.js
@@ -0,0 +1,24 @@
+if (process.env.NODE_ENV !== "production") {
+   require("dotenv").config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'))
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mybrary', { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(() => console.log("connected to MongoDB"))
+   .catch(err => console.log(err))
+
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT || 3000);
